637ff7af81e3ec7eeb5ff9e4e37f801362069bea
Anirban Maitra <anirbanmaitra@Anirbans-MacBook-Air.local>
2023-09-01 23:23:30 +0530

    created validation set function @ utils.py

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def preprocess_data(data):
 
 # Split data into 50% train and 50% test subsets
 def split_data(x, y, test_size, random_state = 1):
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.5, random_state = random_state)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = test_size, random_state = random_state)
     return X_train, y_train, X_test, y_test
 
 #train the model of choice with model parameter
@@ -29,3 +29,11 @@ def train_model(x, y, model_params, model_type):
     #pdb.set_trace()
     model.fit(x, y)
     return model
+
+def train_test_dev_split(x, y, test_size, random_state = 1):
+    dev_ratio = 0.10
+
+    X_train, y_train, X_test, y_test = train_test_split(x, y, test_size = test_size, random_state = random_state) 
+    #spliting the training set into training set and validation set
+    X_train, y_train, X_dev, y_dev = train_test_split(X_train, y_train, test_size = dev_ratio, random_state = random_state)
+    return X_train, y_train, X_test, y_test, X_dev, y_dev
