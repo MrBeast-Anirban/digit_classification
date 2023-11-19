e273f77a9bcc5a792f41b9ceeb4c9c9f3b8b653c
Anirban Maitra <anirbanmaitra@Anirbans-MacBook-Air.local>
2023-11-19 14:54:28 +0530

    quiz 4

---
+++ api/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from joblib import load
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World"
+
+@app.route("/", methods=["POST"])
+def hello_world_post():    
+    return {"op" : "Hello, World POST " + request.json["suffix"]}
+
+@app.route('/predict', methods=['POST'])
+def pred_model():
+    js = request.get_json()
+    image1 = [js['image']]
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../models/treemax_depth:100.joblib')
+    model = load(filename)
+    pred1 = model.predict(image1)
+    return jsonify(prediction=pred1.tolist())
+    
+if __name__ == '__main__':
+    app.run(debug=True)
